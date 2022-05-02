8454d89e5eb8fef81c9009bac6c266f05de0c422
emmegiesse <martagiulia.scoditti@gmail.com>
2022-05-02 21:46:40 +0200

    algo 1 0205

--- scripts/factories/recipes.js
+++ scripts/factories/recipes.js
@@ -1,61 +1,30 @@
 function recipesFactory(data) {
-    const { id, name, servings, ingredients, ingredient, quantity, unit } = data ;
+    const { id, name, servings, ingredients, ingredient, quantity, unit, time, description, appliance, ustensils } = data ;
 
     function getRecipeCardDOM(recipesSection) {
-
-        //const article = document.createElement( 'article' );
-        //article.className= 'recipeArticle';
-
-        //const recipePic= document.createElement( 'img' );
-        //recipePic.setAttribute("src", name);
-
-        //article.appendChild (recipePic);
-
+        
         recipesSection.innerHTML += `   
-            <p> ${name} </p>
+            <article class='recipeArticle'>
+                <div class='recipeCard'>
+                    <h2 class='recipeName'>${name}</h2>
+                    <span class='recipeTime'><i class='far fa-clock'></i>${time} min</span>
+                </div>
+                <div class='recipeInfo'>
+                    <div class='recipeIngredients'>${ingredients.map(elt => `
+                        <p><b>${elt.ingredient} </b>:
+                        ${ 'quantity' in elt ? elt.quantity : ''}
+                        ${ 'unit' in elt ? elt.unit: ''}</p>`).join(' ')}
+                    </div>
+                    <div class='recipeMethod'>
+                        <span>${description}</span>
+                    </div>
+                </div>  
+            </article>    
         `
-
-/*
-
-
-        const phLink = document.createElement('a');
-        phLink.setAttribute('href', `./photographer.html?id=${id}`);
-        phLink.className = 'phLink';
-        const phPic = document.createElement( 'img' );
-        phPic.setAttribute("src", picture);
-        phPic.setAttribute("alt", `Portait de ${name}`);
-        phPic.className = 'phPic';
-        const phName = document.createElement( 'h2' );
-        phName.textContent = name;
-        phName.className = 'phName';
-        // texte avec détails du photographe = ville + tagline + prix (ref 4 du brief)
-        const phText = document.createElement( 'div' );
-        phText.className = 'phText';
-        const phCity = document.createElement( 'h3' );
-        phCity.textContent = `${city}, ${country}`
-        phCity.className = 'phCity';
-        const phTagline = document.createElement( 'p' );
-        phTagline.textContent = tagline;
-        phTagline.className = 'phTagline';
-        const phPrice = document.createElement( 'p' );
-        phPrice.textContent = `${price}€/jour`
-        phPrice.className = 'phPrice';
-
-        //hiérarchie
-        article.appendChild(phLink)
-        phLink.appendChild(phPic);
-        phLink.appendChild(phName);
-        article.appendChild(phText);
-        phText.appendChild(phCity);
-        phText.appendChild(phTagline);
-        phText.appendChild(phPrice);
-     
-        return (article);
-        */
     }
 
-   
     return {getRecipeCardDOM}
 
 }
+
 
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,42 +1,83 @@
+// Récupère et crée la gallerie des recettes - réf fonction dans /factories_function/recipes.js
 function displayData(recipes) {
     const recipesSection = document.getElementById('recipesGallery');
     recipesSection.innerHTML = "";
     recipes.forEach((recipes) => {
         const recipesModel = recipesFactory(recipes);
         const recipesCardDOM = recipesModel.getRecipeCardDOM(recipesSection);
-        //recipesSection.appendChild(recipesCardDOM);
     });
 };
 
+// Définition de la fonction search -- > récupere seulement les recettes filtrées dans la barre principale
 function search (recipes, word) {
     let recipesFilter = [];
     for (let i = 0; i < recipes.length; i++) {
-        if (recipes[i].name.toLowerCase().includes(word.toLowerCase())) {
-            recipesFilter.push(recipes[i])
+        if (recipes[i].name.toLowerCase().includes(word.toLowerCase())
+        ||recipes[i].description.toLowerCase().includes(word.toLowerCase()))
+        //||recipes[i].ingredients.some(elt => elt.ingredient.toLowerCase().includes(word))) 
+        {recipesFilter.push(recipes[i])
         }
-        else if (recipes[i].description.toLowerCase().includes(word.toLowerCase())) {
-            recipesFilter.push(recipes[i])
+        else {
+            let isAdded = false;
+            for (let j=0; j < recipes[i].ingredients.length ; j++){
+                if (recipes[i].ingredients[j].ingredient.toLowerCase().includes(word.toLowerCase())&&isAdded==false){
+                    isAdded = true;
+                    recipesFilter.push(recipes[i])
+                }
+            }
         }
     }
-    displayData(recipesFilter);
+    return recipesFilter;
 };
 
 function init() {
-    // Récupère les datas des recettes
+    // Récupère les datas des recettes avec une recherche dans la barre principale
     displayData(recipes);
+    getAllIngredients(recipes);
+    getAllAppliances(recipes);
+    getAllUstensils(recipes);
     const searchBar = document.getElementById('searchBarInput');
     searchBar.onchange = function (event) {
         console.log(event.target.value);
         const word = event.target.value; 
         if (word.length >= 3) {
-            search (recipes, word);
+            let recipesFilter = search (recipes, word);
+            displayData(recipesFilter);
+            console.log(recipesFilter)
+            console.log(recipesFilter.length)
+            let ingredientsMatch = getAllIngredients(recipesFilter);
+            fillIngredients(ingredientsMatch);
+            getAllAppliances(recipesFilter);
+            getAllUstensils(recipesFilter);
+            console.log(getAllIngredients)
         }
         else {
             displayData(recipes);
+            
         }
     }
 };
 
-init();
+/*
+cons button 
+let ingredientsExample = document.getElementById('ingredientsExample');
+
+function fillIngredients(ingredients) {
+    let ul = document.createElement('ul');
+    ul.classList.add('listUlIng');
+    this.ingredientsExample.appendChild(ul);
+
+    ingredients.forEach((ingredient) => {
+        let listIngredients = document.createElement('li');
+        
+        ul.appendChild(listIngredients);
+        listIngredients.innerHTML = `${Utils.upperText(ingredient)}`
+        listIngredients.classList.add('list-ingredients');
+        listIngredients.setAttribute('data-filter', `${ingredient}`);
+    });
+}
+*/
+
+init()
 
 
